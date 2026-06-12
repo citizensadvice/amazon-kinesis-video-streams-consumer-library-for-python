75674701740b2c7e272b999ef458e4eff35c43c4
Max Derner <max.derner@citizensadvice.org.uk>
2026-06-12 14:26:01 +0100

    EMBLite management step 1: - added EMBLite as 3rd party install - pointed local scripts to 3p dep - retained local custom ebmlite for comparison

--- amazon_kinesis_video_consumer_library/kinesis_video_streams_parser.py
+++ amazon_kinesis_video_consumer_library/kinesis_video_streams_parser.py
@@ -44,7 +44,7 @@ __author__ = "Dean Colcott <https://www.linkedin.com/in/deancolcott/>"
 import timeit
 import logging
 from threading import Thread
-from amazon_kinesis_video_consumer_library.ebmlite import loadSchema
+from ebmlite import loadSchema
 
 # Init the logger.
 log = logging.getLogger(__name__)
